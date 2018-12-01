122f778f666cebc88fd0859f1f68e70621fd8699
Yuxin Zhou <cn.cindyzhou@gmail.com>
2018-12-01 01:33:50 -0800

    Auth Layout finished

--- src/View/AuthView.java
+++ src/View/AuthView.java
@@ -10,105 +10,134 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-public class AuthView implements ActionListener{
-
+public class AuthView {
     private static final int width = 352;
     private static final int height = 288;
-    public static final int fps = 30;
+    private BufferedImage img1 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private BufferedImage img2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private JList jList;
+    private String[] links = new String[]{"l1", "l2", "l3", "l4", "l5"};
+    private JScrollPane jScrollPane;
     private JFrame jFrame;
-    private JPanel controlPane;
-    private JButton playButton = new JButton(">");
-    private JButton stopButton = new JButton("||");
-    private BufferedImage img1;
-    private BufferedImage img2;
-    private Video video1;
-    private Sound sound1;
 
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        BufferedImage frame = video1.getNextFrame();
-        if (frame != null) {
-            imageUtil.copyImage(frame, img1);
-            jFrame.repaint();
-        }
-    }
 
     public AuthView() {
-        img1 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        jFrame = new JFrame();
-        controlPane = new JPanel();
-        video1 = new Video("AIFilmTwo");
-        sound1 = new Sound();
 
-        stopButton.setEnabled(false);
 
-        imageUtil.copyImage(video1.getNextFrame(), img1);
+        // Import panel
+        JLabel label1 = new JLabel("  Actions");
+        JButton importButton = new JButton("Import Primary Video");
+        JButton importButton2 = new JButton("Import Secondary Video");
+        JButton newButton = new JButton("Create Mew Hyperlink");
+
+
+        // Hyper Link List
+        JLabel label2 = new JLabel("Links");
+        jList = new JList();
+        jList.setListData(links);
+        jList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) { // double clicked
+                    JList myList = (JList) e.getSource();
+                    int index = myList.getSelectedIndex();
+                    Object obj = myList.getModel().getElementAt(index);
+                    System.out.println(obj.toString());
+                }
+            }
+        });
+
+        jScrollPane = new JScrollPane();
+        jScrollPane.setPreferredSize(new java.awt.Dimension(200, 80));
+        jScrollPane.getViewport().setView(jList);
+
+        JButton saveButton = new JButton("Save File");
+        JButton connectButton = new JButton("Connect Video");
+
+
+
+        // Left Video
         JLabel lbIm1 = new JLabel(new ImageIcon(img1));
+        lbIm1.setPreferredSize(new java.awt.Dimension(width + 20, height + 20));
+        JSlider jSlider = new JSlider(0, 9000);
+        jSlider.setPaintTicks(true);
 
-        sound1.loadSound("data/AIFilmTwo/AIFilmTwo.wav");
+        // Right Video
+        JLabel lbIm2 = new JLabel(new ImageIcon(img2));
+        lbIm2.setPreferredSize(new java.awt.Dimension(width + 20, height + 20));
+        JSlider jSlider2 = new  JSlider(0, 9000);
+        jSlider2.setPaintTicks(true);
 
-        // Define buttons' function
-        playButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent event) {
-                stopButton.setEnabled(true);
-                playButton.setEnabled(false);
-                try {
-                    sound1.resume();
-                    video1.resume();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        stopButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent event) {
-                stopButton.setEnabled(false);
-                playButton.setEnabled(true);
-                try {
-                    sound1.pause();
-                    video1.pause();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        // Add buttons to control panel
-        controlPane.add(playButton);
-        controlPane.add(stopButton);
-
+        jFrame = new JFrame();
         GridBagLayout gLayout = new GridBagLayout();
         jFrame.getContentPane().setLayout(gLayout);
 
         // Layout
         GridBagConstraints c = new GridBagConstraints();
+
         c.fill = GridBagConstraints.HORIZONTAL;
         c.anchor = GridBagConstraints.CENTER;
-        c.weightx = 0.5;
         c.gridx = 0;
         c.gridy = 0;
-        jFrame.getContentPane().add(lbIm1, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.anchor = GridBagConstraints.CENTER;
-        c.weightx = 0.5;
+        jFrame.getContentPane().add(label1, c);
         c.gridx = 0;
         c.gridy = 1;
+        jFrame.getContentPane().add(importButton, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        jFrame.getContentPane().add(importButton2, c);
+        c.gridx = 0;
+        c.gridy = 3;
+        jFrame.getContentPane().add(newButton, c);
 
-        jFrame.getContentPane().add(controlPane, c);
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        jFrame.getContentPane().add(label2, c);
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridheight = 3;
+        jFrame.getContentPane().add(jScrollPane, c);
+
+        c.gridx = 3;
+        c.gridy = 0;
+        c.gridheight = 2;
+        c.gridwidth = 1;
+        c.ipady = 15;
+        jFrame.getContentPane().add(connectButton, c);
+
+        c.gridx = 3;
+        c.gridy = 2;
+        jFrame.getContentPane().add(saveButton, c);
+
+        c.gridx = 0;
+        c.gridy = 4;
+        c.gridwidth = 2;
+        c.ipady = 0;
+        jFrame.getContentPane().add(lbIm1, c);
+
+        c.gridx = 2;
+        c.gridy = 4;
+        c.gridwidth = 2;
+        jFrame.getContentPane().add(lbIm2, c);
+
+        c.gridx = 0;
+        c.gridy = 9;
+        jFrame.getContentPane().add(jSlider, c);
+
+
+        c.gridx = 2;
+        c.gridy = 9;
+        jFrame.getContentPane().add(jSlider2, c);
 
         jFrame.pack();
         jFrame.setVisible(true);
     }
 
+
     public static void main(String[] args) {
         AuthView view = new AuthView();
-        Timer clock = new Timer(1000/fps, view);
-        clock.start();
+
     }
 
 }
---
+++ src/View/PlayerView.java
@@ -0,0 +1,117 @@
+package View;
+
+import Model.Sound;
+import Model.Video;
+import Model.imageUtil;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+public class PlayerView implements ActionListener {
+
+    private static final int width = 352;
+    private static final int height = 288;
+    public static final int fps = 30;
+    private JFrame jFrame;
+    private JPanel controlPane;
+    private JButton playButton = new JButton(">");
+    private JButton stopButton = new JButton("||");
+    private BufferedImage img1;
+    private BufferedImage img2;
+    private Video video1;
+    private Sound sound1;
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        BufferedImage frame = video1.getNextFrame();
+        if (frame != null) {
+            imageUtil.copyImage(frame, img1);
+            jFrame.repaint();
+        }
+    }
+
+    public PlayerView() {
+        img1 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        jFrame = new JFrame();
+        controlPane = new JPanel();
+        video1 = new Video("AIFilmTwo");
+        sound1 = new Sound();
+
+        stopButton.setEnabled(false);
+
+        imageUtil.copyImage(video1.getNextFrame(), img1);
+        JLabel lbIm1 = new JLabel(new ImageIcon(img1));
+
+        sound1.loadSound("data/AIFilmTwo/AIFilmTwo.wav");
+
+        // Define buttons' function
+        playButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent event) {
+                stopButton.setEnabled(true);
+                playButton.setEnabled(false);
+                try {
+                    sound1.resume();
+                    video1.resume();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        stopButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent event) {
+                stopButton.setEnabled(false);
+                playButton.setEnabled(true);
+                try {
+                    sound1.pause();
+                    video1.pause();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Add buttons to control panel
+        controlPane.add(playButton);
+        controlPane.add(stopButton);
+
+        GridBagLayout gLayout = new GridBagLayout();
+        jFrame.getContentPane().setLayout(gLayout);
+
+        // Layout
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        jFrame.getContentPane().add(lbIm1, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 1;
+
+        jFrame.getContentPane().add(controlPane, c);
+
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        PlayerView view = new PlayerView();
+        Timer clock = new Timer(1000/fps, view);
+        clock.start();
+    }
+
+}
+
