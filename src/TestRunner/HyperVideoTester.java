65aafef31b9c1a382a28d2918127f84a925d0474
Tianyang Wen <kirawen@umich.edu>
2018-12-03 14:04:49 -0800

    added file helper class

--- src/TestRunner/HyperVideoTester.java
+++ src/TestRunner/HyperVideoTester.java
@@ -1,6 +1,7 @@
 package TestRunner;
 
 import Model.HyperVideo;
+import Model.HyperVideoFileHelper;
 import Model.HyperVideoLink;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -15,6 +16,8 @@ import java.util.Set;
 
 public class HyperVideoTester {
     public HyperVideoTester() throws IOException, ParseException {
+        HyperVideoFileHelper fileHelper = new HyperVideoFileHelper();
+
         HyperVideo h_video = new HyperVideo("new video", "USCOne");
 
         ArrayList<Integer> tmp_set1 = new ArrayList<Integer>();
@@ -26,41 +29,25 @@ public class HyperVideoTester {
         h_video.addHyperLink("link1", 0, tmp_set1, "lalala", 10);
 
         ArrayList<Integer> tmp_set2 = new ArrayList<Integer>();
-        tmp_set1.add(101);
-        tmp_set1.add(102);
-        tmp_set1.add(110);
-        tmp_set1.add(120);
+        tmp_set2.add(101);
+        tmp_set2.add(102);
+        tmp_set2.add(110);
+        tmp_set2.add(120);
 
         h_video.addHyperLink("link2", 0, tmp_set2, "lala", 10);
 
         ArrayList<Integer> tmp_set3 = new ArrayList<Integer>();
-        tmp_set1.add(201);
-        tmp_set1.add(202);
-        tmp_set1.add(210);
-        tmp_set1.add(220);
+        tmp_set3.add(201);
+        tmp_set3.add(202);
+        tmp_set3.add(210);
+        tmp_set3.add(220);
 
         h_video.addHyperLink("link1", 10, tmp_set3, "lalala", 50);
 
-        JSONObject jo = h_video.returnJSON();
-
-        PrintWriter pw = new PrintWriter("JSONExample.json");
-        pw.write(jo.toJSONString());
-
-        pw.flush();
-        pw.close();
-
-        JSONParser parser = new JSONParser();
-        JSONObject importJO = (JSONObject) parser.parse(new FileReader("JSONExample.json"));
-
-        HyperVideo h2_video = new HyperVideo(importJO);
+        fileHelper.saveHyperVideoToFile(h_video, "first_save.JSON");
+        HyperVideo h2_video = fileHelper.readHyperVideoFromFile("first_save.JSON");
         h2_video.addHyperLink("link1", 100, tmp_set3, "lalala", 50);
-        JSONObject exportJO = h2_video.returnJSON();
-
-        PrintWriter pw2 = new PrintWriter("JSONExample2.json");
-        pw2.write(exportJO.toJSONString());
-
-        pw2.flush();
-        pw2.close();
+        fileHelper.saveHyperVideoToFile(h2_video, "second_save.JSON");
     }
     public static void main(String[] args) throws IOException, ParseException {
         HyperVideoTester tester = new HyperVideoTester();
